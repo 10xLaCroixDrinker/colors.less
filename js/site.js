73ee38e5e1b044a298719f42f3fd09ea00df21c5
Jimmy King <hello@jimmyking.me>
2012-10-31 15:57:05 -0700

    Switched to @hbi99's fork of less-1.1.6.js to fully impliment color picker in demo

--- js/site.js
+++ js/site.js
@@ -1,19 +1,16 @@
 window.onload = function() {
-	var swatch = document.getElementById("testSwatch");
+	var baseColor;
 	picker = new Color.Picker({
 		color: "#FD00FF", // accepts rgba(), or #hex
 		display: true,
 		size: 200,
 		callback: function(rgba, state, type) {
-			swatch.style.background = Color.Space(rgba, "RGBA>W3");
+			baseColor = Color.Space(rgba, "RGBA>W3");
+			less.modifyVars({
+        '@baseColor': baseColor
+      });
 		}
 	});
 	picker.element.style.top = swatch.offsetTop + "px";
 	picker.element.style.left = (swatch.offsetLeft - 285) + "px";
-	
-	var less = require('less');
-
-  less.render('@baseColor: ' + Color.Space(rgba, "RGBA>W3") + ';', function (e, css) {
-    console.log(css);
-  });
 };
